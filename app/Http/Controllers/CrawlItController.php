81d264a298740d6c680836737860e9258bdb1f24
root <root@DESKTOP-2HCQ59E.localdomain>
2022-01-19 22:25:51 -0500

    Optimizing the source code

--- app/Http/Controllers/CrawlItController.php
+++ app/Http/Controllers/CrawlItController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+
 use DOMDocument;
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\HelpersController;
 class CrawlItController extends Controller
 {
 
     /**
-     * Home page
+     * home()
+     * This is juste a function to return the homepage view to the user
+     *
+     * @return void
      */
     public function home(){
         return view("home");
@@ -20,19 +24,19 @@ class CrawlItController extends Controller
      * Main crawler function called by AJAX
      *
      * @param Request $request
-     * @return void
+     * @return JSON
      */
     public function crawl(Request $request){
         
         // Get the starting page http code
-        if ($this::getHeaders($request->url)=="200"){
+        if (HelpersController::getHeaders($request->url)=="200"){
             // We are good to go and can crawl this website
 
             // Get the HTML for the starting page
-            $start = $this::getHTML($request->url);
+            $start = HelpersController::getHTML($request->url);
 
             // Get word count
-            $words = $this::wordCount($start['html']);
+            $words = HelpersController::wordCount($start['html']);
             $totalWords = 0;
             foreach ($words as $key=>$word) {
                 $totalWords += $word;
@@ -40,15 +44,15 @@ class CrawlItController extends Controller
 
             // Get data for the starting page
             $links = array(array(
-                    "pageTitle" => ($this::getTitle($start['html'])?$this::getTitle($start['html']):$request->url),
+                    "pageTitle" => (HelpersController::getTitle($start['html'])?HelpersController::getTitle($start['html']):$request->url),
                     "linkTitle"=> "",
                     "href" => $request->url,
-                    "code" => $this::getHeaders($request->url),
+                    "code" => HelpersController::getHeaders($request->url),
                     "int_ext" => "int",
                     "time" => round($start['time'],3),
                     "html" => base64_encode($start['html']),
-                    "img" => $this::getImages($start['html']),
-                    "img_count" => count($this::getImages($start['html'])),
+                    "img" => HelpersController::getImages($start['html']),
+                    "img_count" => count(HelpersController::getImages($start['html'])),
                     "word_count" => count($words),
                     "words" => $words,
                     "total_words"=> $totalWords
@@ -61,7 +65,7 @@ class CrawlItController extends Controller
 
             // Get all links from this HTML & build sub page data
             if ($max>=1){
-                $links_found = $this::hrefCrawl($start['html'], $request->url, $request->pages);
+                $links_found = HelpersController::hrefCrawl($start['html'], $request->url, $request->pages);
                 $links = array_merge($links, $links_found);
             }
 
@@ -75,7 +79,7 @@ class CrawlItController extends Controller
             foreach($links as $key=>$link){
 
                 // Lets add the missing link data to the array
-                $count = $this::hrefCount(base64_decode($link['html']), $link['href']);
+                $count = HelpersController::hrefCount(base64_decode($link['html']), $link['href']);
                 $links[$key]['int_count'] = count($count['int']);
                 $links[$key]['int_links'] = $count['int'];
                 $links[$key]['ext_count'] = count($count['ext']);
@@ -132,301 +136,12 @@ class CrawlItController extends Controller
             $out['res']['title_avr'] =  $sumTitle/$titleCount;
             $out['res']['words'] = $wordArray;
             $out['data'] = $links;       
-
-            //dd($this::datatableOutput($out));
             
             // Return json back to the browser
-            return response()->json($this::datatableOutput($out));
+            return response()->json(HelpersController::datatableOutput($out));
 
         }
 
     }
-
-    /**
-     * wordCount()
-     * Not 100% accurate but it's a start. With time, I could review this function and get a better output of words
-     */
-    public static function wordCount($html){
-
-        // Get rid of style, script etc
-        $search = array('@<script[^>]*?>.*?</script>@si',  // Strip out javascript
-                '@<head>.*?</head>@siU',            // Lose the head section
-                '@<style[^>]*?>.*?</style>@siU',    // Strip style tags properly
-                '@<![\s\S]*?--[ \t\n\r]*>@'         // Strip multi-line comments including CDATA
-        );
-
-        $contents = preg_replace($search, '', $html); 
-        $result = array_count_values(str_word_count(strip_tags($contents), 1));
-
-        return $result;
-
-    }
-
-    /**
-     * datatableOutput()
-     * Retuns the expected achitechture of the array for datatable
-     *
-     * @param [array] $array
-     * @return void
-     */
-    public static function datatableOutput($array){
-
-        foreach($array['data'] as $key=>$line){
-            $output[] = [
-                $line['href'],
-                $line['code'],
-                $line['time'],
-                $line['img_count'],
-                $line['int_count'],
-                $line['ext_count'],
-                $line['word_count'],
-                $line['total_words'],
-                $line['img']
-            ];
-        }
-
-        return array('data'=> $output,'res'=> $array['res']);
-    }
-
-    /**
-     * getHTML()
-     * Return the HTML of a remote URL
-     * 
-     * @param [string] $url
-     * @return void
-     */
-    public static function getHTML($url){
-        $time_start = microtime(true); 
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        $output["html"] = curl_exec($curl);
-        curl_close($curl);
-        $time_end = microtime(true);
-        $output["time"] = round(($time_end - $time_start), 3);
-        return $output;
-    }
-
-    /**
-     * getHeaders()
-     * Returns the HTTP code of a URL
-     *
-     * @param [string] $url
-     * @return void
-     */
-    public static function getHeaders($url){
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_HEADER, true);    // we want headers
-        curl_setopt($curl, CURLOPT_NOBODY, true);    // we don't need body
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($curl, CURLOPT_TIMEOUT,10);
-        $output = curl_exec($curl);
-        $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        curl_close($curl);
-        return ($httpcode?$httpcode:false);
-    }
-
-    /**
-     * hrefCrawl()
-     * Returns an array of links found in an HTML string
-     *
-     * @param [string] $html
-     * @param [string] $startURL
-     * @param [int] $max
-     * @param [string] standard (Default) | count
-     * @return void
-     */
-    public static function hrefCrawl($html, $startURL, $max){
-
-        // Retreive info on the start URL to fix links we retreive
-        $parseStart = parse_url($startURL);
-        $baseStart = $parseStart["scheme"].'://'.$parseStart["host"];
-
-        $htmlDom = new DOMDocument;
-        @$htmlDom->loadHTML($html);
-
-        //Extract the links from the HTML.
-        $links = $htmlDom->getElementsByTagName('a');
-        $extractedLinks = array();
-
-        //Loop through the DOMNodeList.
-        $i = 1;
-        foreach($links as $link){
-
-            // Only scan for the number of pages asked for
-            if ($i>=$max){
-                continue;
-            }else{
-
-                // No duplicates
-                if (!in_array($link, $extractedLinks)){
-
-                    $linkText = $link->nodeValue;
-                    $linkHref = $link->getAttribute('href');
-
-                    //If the link is empty, skip it and don't
-                    //add it to our $extractedLinks array
-                    if(strlen(trim($linkHref)) == 0){
-                        continue;
-                    }
-
-                    //Skip if it is a hashtag / anchor link.
-                    if($linkHref[0] == '#'){
-                        continue;
-                    }
-
-                    // Fixing links
-                    // Check if link is missing the base URL
-                    $parseCurrent = parse_url($linkHref);
-                    if (!isset($parseCurrent["scheme"])||!isset($parseCurrent["host"])){
-                        $linkHref = $baseStart.$parseCurrent["path"];
-                        $parseCurrent = parse_url($linkHref);
-                    }
-
-                    // Skip if the link is equal to the starting URL
-                    // Compare with out trailing slashes
-                    if(rtrim($linkHref, '/') == rtrim($startURL, '/')){
-                        continue;
-                    }
-
-                    $code = CrawlItController::getHeaders($linkHref);
-                    $currentHTML = CrawlItController::getHTML($linkHref);
-
-                    // Get word count
-                    $words = CrawlItController::wordCount($currentHTML['html']);
-                    $totalWords = 0;
-                    foreach ($words as $key=>$word) {
-                        $totalWords += $word;
-                    }
-
-                    //Add the link to our $extractedLinks array.
-                    $extractedLinks[] = array(
-                        'pageTitle' => CrawlItController::getTitle($currentHTML['html']),
-                        'linkTitle'=> trim(preg_replace("/\s+/"," ",$linkText)),
-                        'href' => $linkHref,
-                        'code' => $code,
-                        'int_ext' => ($parseCurrent["host"]==$parseStart["host"]?"int":"ext"),
-                        'time' => round($currentHTML['time'],3),
-                        'html' => base64_encode($currentHTML['html']),
-                        "img" =>CrawlItController::getImages($currentHTML['html']),
-                        "img" => CrawlItController::getImages($currentHTML['html']),
-                        "img_count" => count(CrawlItController::getImages($currentHTML['html'])),
-                        "word_count" => count($words),
-                        "words" => $words,
-                        "total_words"=> $totalWords
-                    );
-
-
-                }
-
-                $i++;
-            }
-
-        }
-
-        return $extractedLinks;
-    }
-
-    public static function hrefCount($html, $startURL){
-
-        // Retreive info on the start URL to fix links we retreive
-        $parseStart = parse_url($startURL);
-        $baseStart = $parseStart["scheme"].'://'.$parseStart["host"];
-
-        $htmlDom = new DOMDocument;
-        @$htmlDom->loadHTML($html);
-
-        //Extract the links from the HTML.
-        $links = $htmlDom->getElementsByTagName('a');
-        $extractedLinks = array();
-
-        //Loop through the DOMNodeList.
-        $i = 1;
-        foreach($links as $link){
-
-            // No duplicates
-            if (!in_array($link, $extractedLinks)){
-
-                $linkHref = $link->getAttribute('href');
-
-                //If the link is empty, skip it and don't
-                //add it to our $extractedLinks array
-                if(strlen(trim($linkHref)) == 0){
-                    continue;
-                }
-
-                //Skip if it is a hashtag / anchor link.
-                if($linkHref[0] == '#'){
-                    continue;
-                }
-
-                // Fixing links
-                // Check if link is missing the base URL
-                $parseCurrent = parse_url($linkHref);
-                if (!isset($parseCurrent["scheme"])||!isset($parseCurrent["host"])){
-                    $linkHref = $baseStart.$parseCurrent["path"];
-                    $parseCurrent = parse_url($linkHref);
-                }
-
-                // Skip if the link is equal to the starting URL
-                // Compare with out trailing slashes
-                if(rtrim($linkHref, '/') == rtrim($startURL, '/')){
-                    continue;
-                }
-
-                //Add the link to our $extractedLinks array.
-                $extractedLinks[($parseCurrent["host"]==$parseStart["host"]?"int":"ext")][] = array(
-                    'href' => $linkHref
-                );
-
-            }
-
-            $i++;
-
-        }
-
-        return $extractedLinks;
-    }
-
-    /**
-     * getTitle()
-     * Returns the content found in a <title> tag
-     *
-     * @param [string] $html
-     * @return void
-     */
-    public static function getTitle($html){
-        $htmlDom = new DOMDocument;
-        @$htmlDom->loadHTML($html);
-        
-        $list = $htmlDom->getElementsByTagName("title");
-        if ($list->length > 0) {
-            return trim(preg_replace("/\s+/"," ",$list->item(0)->textContent));
-        }
-     
-        return false;
-    }
-
-    /**
-     * getImages()
-     * Returns image info found in HTML
-     */
-
-     public static function getImages($html){
-
-        $img = array();
-        $htmlDom = new DOMDocument;
-        @$htmlDom->loadHTML($html);
-
-        //Extract the links from the HTML.
-        $tags = $htmlDom->getElementsByTagName('img');
-
-        foreach ($tags as $tag) {
-            if (!in_array($tag->getAttribute('src'), $img) && $tag->getAttribute('src')!=""){
-                $img[] = $tag->getAttribute('src');
-            }
-        }
-
-        return $img;
-     }
 
 }
---
+++ app/Http/Controllers/HelpersController.php
@@ -0,0 +1,308 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DOMDocument;
+use Illuminate\Http\Request;
+
+class HelpersController extends Controller
+{
+    /**
+     * wordCount()
+     * Not 100% accurate but it's a start. With time, I could review this function and get a better output of words
+     * 
+     * @param [string] $html
+     */
+    public static function wordCount($html){
+
+        // Get rid of style, script etc
+        $search = array('@<script[^>]*?>.*?</script>@si',  // Strip out javascript
+                '@<head>.*?</head>@siU',            // Lose the head section
+                '@<style[^>]*?>.*?</style>@siU',    // Strip style tags properly
+                '@<![\s\S]*?--[ \t\n\r]*>@'         // Strip multi-line comments including CDATA
+        );
+
+        // This function could defenetly be improved for accuracy but it's still a good start for this challenge
+
+        $contents = preg_replace($search, '', $html); 
+        $result = array_count_values(str_word_count(strip_tags($contents), 1));
+
+        return $result;
+
+    }
+
+    /**
+     * datatableOutput()
+     * Retuns the expected achitechture of the array for datatable
+     *
+     * @param [array] $array
+     * @return void
+     */
+    public static function datatableOutput($array){
+
+        foreach($array['data'] as $key=>$line){
+            $output[] = [
+                $line['href'],
+                $line['code'],
+                $line['time'],
+                $line['img_count'],
+                $line['int_count'],
+                $line['ext_count'],
+                $line['word_count'],
+                $line['total_words'],
+                $line['img']
+            ];
+        }
+
+        return array('data'=> $output,'res'=> $array['res']);
+    }
+
+    /**
+     * getHTML()
+     * Return the HTML of a remote URL
+     * 
+     * @param [string] $url
+     * @return void
+     */
+    public static function getHTML($url){
+        $time_start = microtime(true); 
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        $output["html"] = curl_exec($curl);
+        curl_close($curl);
+        $time_end = microtime(true);
+        $output["time"] = round(($time_end - $time_start), 3);
+        return $output;
+    }
+
+    /**
+     * getHeaders()
+     * Returns the HTTP code of a URL
+     *
+     * @param [string] $url
+     * @return void
+     */
+    public static function getHeaders($url){
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HEADER, true);    // we want headers
+        curl_setopt($curl, CURLOPT_NOBODY, true);    // we don't need body
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($curl, CURLOPT_TIMEOUT,10);
+        $output = curl_exec($curl);
+        $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+        return ($httpcode?$httpcode:false);
+    }
+
+    /**
+     * hrefCrawl()
+     * Search and find href links in HTML and crawl them up to $max
+     *
+     * @param [string] $html
+     * @param [string] $startURL
+     * @param [int] $max
+     * @return $extractedLinks array()
+     */
+    public static function hrefCrawl($html, $startURL, $max){
+
+        // Retreive info on the start URL to fix links we retreive
+        $parseStart = parse_url($startURL);
+        $baseStart = $parseStart["scheme"].'://'.$parseStart["host"];
+
+        $htmlDom = new DOMDocument;
+        @$htmlDom->loadHTML($html);
+
+        //Extract the links from the HTML.
+        $links = $htmlDom->getElementsByTagName('a');
+        $extractedLinks = array();
+
+        //Loop through the DOMNodeList.
+        $i = 1;
+        foreach($links as $link){
+
+            // Only scan for the number of pages asked for
+            if ($i>=$max){
+                continue;
+            }else{
+
+                // No duplicates
+                if (!in_array($link, $extractedLinks)){
+
+                    $linkText = $link->nodeValue;
+                    $linkHref = $link->getAttribute('href');
+
+                    //If the link is empty, skip it and don't
+                    //add it to our $extractedLinks array
+                    if(strlen(trim($linkHref)) == 0){
+                        continue;
+                    }
+
+                    //Skip if it is a hashtag / anchor link.
+                    if($linkHref[0] == '#'){
+                        continue;
+                    }
+
+                    // Fixing links
+                    // Check if link is missing the base URL
+                    $parseCurrent = parse_url($linkHref);
+                    if (!isset($parseCurrent["scheme"])||!isset($parseCurrent["host"])){
+                        $linkHref = $baseStart.$parseCurrent["path"];
+                        $parseCurrent = parse_url($linkHref);
+                    }
+
+                    // Skip if the link is equal to the starting URL
+                    // Compare with out trailing slashes
+                    if(rtrim($linkHref, '/') == rtrim($startURL, '/')){
+                        continue;
+                    }
+
+                    $code = HelpersController::getHeaders($linkHref);
+                    $currentHTML = HelpersController::getHTML($linkHref);
+
+                    // Get word count
+                    $words = HelpersController::wordCount($currentHTML['html']);
+                    $totalWords = 0;
+                    foreach ($words as $key=>$word) {
+                        $totalWords += $word;
+                    }
+
+                    //Add the link to our $extractedLinks array.
+                    $extractedLinks[] = array(
+                        'pageTitle' => HelpersController::getTitle($currentHTML['html']),
+                        'linkTitle'=> trim(preg_replace("/\s+/"," ",$linkText)),
+                        'href' => $linkHref,
+                        'code' => $code,
+                        'int_ext' => ($parseCurrent["host"]==$parseStart["host"]?"int":"ext"),
+                        'time' => round($currentHTML['time'],3),
+                        'html' => base64_encode($currentHTML['html']),
+                        "img" =>HelpersController::getImages($currentHTML['html']),
+                        "img" => HelpersController::getImages($currentHTML['html']),
+                        "img_count" => count(HelpersController::getImages($currentHTML['html'])),
+                        "word_count" => count($words),
+                        "words" => $words,
+                        "total_words"=> $totalWords
+                    );
+
+
+                }
+
+                $i++;
+            }
+
+        }
+
+        return $extractedLinks;
+    }
+
+    /**
+     * hrefCount()
+     * 
+     * @param [string] $html
+     * @param [string] $startURL
+     * @return $extractedLinks array()
+     */
+    public static function hrefCount($html, $startURL){
+
+        // Retreive info on the start URL to fix links we retreive
+        $parseStart = parse_url($startURL);
+        $baseStart = $parseStart["scheme"].'://'.$parseStart["host"];
+
+        $htmlDom = new DOMDocument;
+        @$htmlDom->loadHTML($html);
+
+        //Extract the links from the HTML.
+        $links = $htmlDom->getElementsByTagName('a');
+        $extractedLinks = array();
+
+        //Loop through the DOMNodeList.
+        $i = 1;
+        foreach($links as $link){
+
+            // No duplicates
+            if (!in_array($link, $extractedLinks)){
+
+                $linkHref = $link->getAttribute('href');
+
+                //If the link is empty, skip it and don't
+                //add it to our $extractedLinks array
+                if(strlen(trim($linkHref)) == 0){
+                    continue;
+                }
+
+                //Skip if it is a hashtag / anchor link.
+                if($linkHref[0] == '#'){
+                    continue;
+                }
+
+                // Fixing links
+                // Check if link is missing the base URL
+                $parseCurrent = parse_url($linkHref);
+                if (!isset($parseCurrent["scheme"])||!isset($parseCurrent["host"])){
+                    $linkHref = $baseStart.$parseCurrent["path"];
+                    $parseCurrent = parse_url($linkHref);
+                }
+
+                // Skip if the link is equal to the starting URL
+                // Compare with out trailing slashes
+                if(rtrim($linkHref, '/') == rtrim($startURL, '/')){
+                    continue;
+                }
+
+                //Add the link to our $extractedLinks array.
+                $extractedLinks[($parseCurrent["host"]==$parseStart["host"]?"int":"ext")][] = array(
+                    'href' => $linkHref
+                );
+
+            }
+
+            $i++;
+
+        }
+
+        return $extractedLinks;
+    }
+
+    /**
+     * getTitle()
+     * Returns the content found in a <title> tag
+     *
+     * @param [string] $html
+     * @return string | false
+     */
+    public static function getTitle($html){
+        $htmlDom = new DOMDocument;
+        @$htmlDom->loadHTML($html);
+        
+        $list = $htmlDom->getElementsByTagName("title");
+        if ($list->length > 0) {
+            return trim(preg_replace("/\s+/"," ",$list->item(0)->textContent));
+        }
+     
+        return false;
+    }
+
+    /**
+     * getImages()
+     * Returns image info found in HTML
+     * 
+     * @param [string] $html
+     * @return $img array
+     */
+     public static function getImages($html){
+
+        $img = array();
+        $htmlDom = new DOMDocument;
+        @$htmlDom->loadHTML($html);
+
+        //Extract the links from the HTML.
+        $tags = $htmlDom->getElementsByTagName('img');
+
+        foreach ($tags as $tag) {
+            if (!in_array($tag->getAttribute('src'), $img) && $tag->getAttribute('src')!=""){
+                $img[] = $tag->getAttribute('src');
+            }
+        }
+
+        return $img;
+     }
+}
